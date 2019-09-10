e839b5a25e8596c2a95cdf1449624f82367ff251
nabz123 <53206514+nabz123@users.noreply.github.com>
2019-09-10 16:12:27 +1200

    Update rain.bat

--- rain.bat
+++ rain.bat
@@ -1,21 +1,25 @@
 @echo off
-mode con: cols=187 lines=44
-color a
-: colorful
-title 
-echo [90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m
-title C
-echo [91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[90m%random%[0m
-title CO
-echo [92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[90m%random%[0m[91m%random%[0m
-title COO
-echo [93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[90m%random%[0m[91m%random%[0m[93m%random%[0m
-title COOL
-echo [94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[90m%random%[0m[91m%random%[0m[93m%random%[0m[92m%random%[0m
-title COO
-echo [95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[90m%random%[0m[91m%random%[0m[93m%random%[0m[92m%random%[0m[94m%random%[0m
-title CO
-echo [96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[90m%random%[0m[91m%random%[0m[93m%random%[0m[92m%random%[0m[94m%random%[0m[95m%random%[0m
-title C
-echo [97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[97m%random%[0m[90m%random%[0m[91m%random%[0m[92m%random%[0m[93m%random%[0m[94m%random%[0m[95m%random%[0m[96m%random%[0m[90m%random%[0m[91m%random%[0m[93m%random%[0m[92m%random%[0m[94m%random%[0m[95m%random%[0m[97m%random%[0m
-goto colorful
+setlocal EnableDelayedExpansion
+set "timeout=2"
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set "DEL=%%a"
+)
+:loop
+FOR %%G IN (9,a,b,c,d,e) DO (
+::[HELLO
+::[WORLD
+::]RED
+cls
+for /f "delims=:[ tokens=*" %%A in ('findstr /b "::[" "%~f0"') do @call :ColorText 0%%G "%%A"
+for /f "delims=:] tokens=*" %%A in ('findstr /b "::]" "%~f0"') do @call :ColorText 0c "%%A"
+ping -n !timeout! 127.0.0.1 > nul
+)
+goto loop
+
+:ColorText
+echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1
+echo.
+goto :eof
